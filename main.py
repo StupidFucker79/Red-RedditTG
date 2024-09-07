813b49bddd9213a640ecb7999aa5cdabdf2b9ee2
mrspidy <74917158+MrxAravind@users.noreply.github.com>
2024-09-07 06:59:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ async def download_redgif(link):
 # Async main function to process subreddit images
 async def main():
  async with app:
-    subreddit_name = 'braless'  # replace with your target subreddit
+    subreddit_name = 'BlowjobGradeA'  # replace with your target subreddit
     urls = await get_urls(subreddit_name)
     uploaded_urls = []
     for url in urls:
